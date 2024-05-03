64b8771848ddafd67ddf50a22d8956190984606d
meng877 <meng_877@outlook.com>
2024-05-03 14:39:49 +0800

    add help

--- datapack_music_player/config.py
+++
@@ -1,8 +0,0 @@
-from mcdreforged.api.all import *
-
-class datapack_music_config(Serializable):
-    datapack_path: str = "./server/world/datapacks/"
-    ignore_datapacks: set = []
-    
-
-
---
+++ datapack_music_player/default_config.py
@@ -0,0 +1,7 @@
+default_config = {
+    #数据包目录
+    "datapack_path": "./server/world/datapacks/",
+    #填写除了音乐之外的zip数据包
+    "ignore_zip": [],
+    "autoplay": False
+}
